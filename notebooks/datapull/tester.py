6631adc6f14f4a0b7c12d915bfcaf1475cc46768
Joe Kupresanin <joekupresanin@u.northwestern.edu>
2019-04-18 08:03:45 -0500

    Switched input / output files to csv.

--- notebooks/datapull/tester.py
+++ notebooks/datapull/tester.py
@@ -13,22 +13,28 @@ month = input("Give the month as 4, 5, 6, 7, 8, or 9: ")
 day = input("Give the day as 1, 2, ..., 29, 30, or 31: ")
 print()
 
+# Need to put decision here for user
+# Are you gambling for today?
+# Or are you just inputting results from the past?
+
 if answer == "YES":
 
     data = daily_data(month, day)
-    with open('./daily_data/outfile_{0}_{1}_pre'.format(month, day), 'wb') as fp:
-        pickle.dump(data, fp)
+    # with open('./daily_data/outfile_{0}_{1}_pre'.format(month, day), 'wb') as fp:
+    #     pickle.dump(data, fp)
+    data.to_csv('./daily_data/outfile_{0}_{1}_pre'.format(month, day), encoding='utf-8')
 
 else:
-    with open('./daily_data/outfile_{0}_{1}_pre'.format(month, day), 'rb') as fp:
-        data = pickle.load(fp)
+    # with open('./daily_data/outfile_{0}_{1}_pre'.format(month, day), 'rb') as fp:
+    #     data = pickle.load(fp)
+    data = pd.read_csv('./daily_data/outfile_{0}_{1}_pre'.format(month, day), encoding='utf-8')
 
 today = get_predicted_runs(data, month, day)
 today = user_input_results(today)
 
 print(today)
 
-with open('./daily_results/results_{0}_{1}'.format(month, day), 'wb') as fp:
-    pickle.dump(today, fp)
+# with open('./daily_results/results_{0}_{1}'.format(month, day), 'wb') as fp:
+#     pickle.dump(today, fp)
 
-
+today.to_csv('./daily_results/results_{0}_{1}'.format(month, day), encoding='utf-8')
