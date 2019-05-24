8a30afff03723e430127b68c24ac97e9f75dd53f
Joe Kupresanin <joekupresanin@u.northwestern.edu>
2019-05-24 11:11:42 -0500

    Created config_model.py only.

--- config_model.py
+++ config_model.py
@@ -1,3 +1,20 @@
+# Modeling methodology
+
+# Each day, 49 randomly selected random forest models are selected.
+# The best one (based on lowest CV R-Sq) is selected and the day's baseball predictions are scored.
+
+# Since predicting baseball scores is nearly impossible, I've taken this as the "best" we can do.
+# 49 models takes about 10 minutes on EC2 2XL instance
+# A randomized state has not been set, though should one want the predictions to consistent, this would be needed.
+
+# It is not uncommon for the prediction to be 9.5643 runs, the bookie to have it at 10 runs...
+# and for the actual outcome to be 2-1 for 3 runs, or 9-4 for 13 runs, or 11-10 for 21 runs.
+
+# Daily RMSE is about 4.00 runs which means if the prediction is 9.5 runs, outcomes in [1, 17] are not unusual.
+
+# Long story short, if baseball runs were predictable, I'd drop out of MSIA and make $3,000,000 over the summer...
+# and retire to Dublin, IE to live a life of wandering the streets, drawing, playing piano, and reading good books.
+
 tree_start = 200
 tree_stop = 3000
 tree_jump = 10
