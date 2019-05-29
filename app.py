24b3b790d3e4b0360f70d6324e9353600450f6a7
Joe Kupresanin <joekupresanin@u.northwestern.edu>
2019-05-28 19:23:05 -0500

    Let's get this Flask going already!

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from flask import render_template, request, redirect, url_for
 from flask import Flask
 # from src.add_songs import Tracks
 import Create_RDS_DB
+#import config_flask as cf
 from flask_sqlalchemy import SQLAlchemy
 
 # Initialize the Flask application
 app = Flask(__name__)
 
 # Configure flask app from flask_config.py
-app.config.from_pyfile('flask_config.py')
+# app.config.from_pyfile('flask_config.py')
 
 # Define LOGGING_CONFIG in flask_config.py - path to config file for setting
 # up the logger (e.g. config/logging/local.conf)
