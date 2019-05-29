8d8cf462247beb5197c7bd699ebfdc9d9ed8960c
Joe Kupresanin <joekupresanin@u.northwestern.edu>
2019-05-28 19:34:50 -0500

    Let's get this Flask going already!

--- config_flask.py
+++ config_flask.py
@@ -4,7 +4,6 @@ DEBUG = True
 # LOGGING_CONFIG = "config/logging/local.conf"
 PORT = 3000
 APP_NAME = "baseball"
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///../data/tracks.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 HOST = "0.0.0.0"
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
