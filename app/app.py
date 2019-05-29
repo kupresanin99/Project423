0d09a962f6de57c5f02116ed952557e7809b0a6d
Joe Kupresanin <joekupresanin@u.northwestern.edu>
2019-05-28 19:04:26 -0500

    Let's get this Flask going already!

---
+++ app/app.py
@@ -0,0 +1,54 @@
+import traceback
+from flask import render_template, request, redirect, url_for
+# import logging.config
+from flask import Flask
+# from src.add_songs import Tracks
+from Create_RDS_DB import Predictions, Reports
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Configure flask app from flask_config.py
+app.config.from_pyfile('../flask_config.py')
+
+# Define LOGGING_CONFIG in flask_config.py - path to config file for setting
+# up the logger (e.g. config/logging/local.conf)
+# logging.config.fileConfig(app.config["LOGGING_CONFIG"])
+# logger = logging.getLogger("penny-lane")
+# logger.debug('Test log')
+
+# Initialize the database
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+
+    try:
+        predictions = db.session.query(Predictions).limit(app.config["MAX_ROWS_SHOW"]).all()
+        # logger.debug("Index page accessed")
+        return render_template('index.html', predictions=predictions)
+    except:
+        traceback.print_exc()
+        # logger.warning("Not able to display tracks, error page returned")
+        return render_template('error.html')
+
+
+# @app.route('/add', methods=['POST'])
+# def add_entry():
+#     """View that process a POST with new song input
+#     :return: redirect to index page
+#     """
+#
+#     try:
+#         track1 = Tracks(artist=request.form['artist'], album=request.form['album'], title=request.form['title'])
+#         db.session.add(track1)
+#         db.session.commit()
+#         logger.info("New song added: %s by %s", request.form['title'], request.form['artist'])
+#         return redirect(url_for('index'))
+#     except:
+#         logger.warning("Not able to display tracks, error page returned")
+#         return render_template('error.html')
+
+
---
+++ config_flask.py
@@ -0,0 +1,19 @@
+import os
+
+DEBUG = True
+# LOGGING_CONFIG = "config/logging/local.conf"
+PORT = 3000
+APP_NAME = "baseball"
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///../data/tracks.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+HOST = "0.0.0.0"
+SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
+MAX_ROWS_SHOW = 100
+
+conn_type = "mysql+pymysql"
+user = os.environ.get("MYSQL_USER")
+password = os.environ.get("MYSQL_PASSWORD")
+host = os.environ.get("MYSQL_HOST")
+port = os.environ.get("MYSQL_PORT")
+DATABASE_NAME = 'msia423'
+SQLALCHEMY_DATABASE_URI = "{}://{}:{}@{}:{}/{}".format(conn_type, user, password, host, port, DATABASE_NAME)
