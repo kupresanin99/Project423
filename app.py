8346515515b93f5d001e4ea48864c3ed2a35e90c
Joe Kupresanin <joekupresanin@u.northwestern.edu>
2019-05-30 07:33:21 -0500

    Webpage up, now for user interface.

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ app = Flask(__name__)
 
 # Configure flask app from flask_config.py
 app.config.from_pyfile('config_flask.py')
-print('user', app.config['user'])
-print('password', app.config['password'])
-print('host', app.config['host'])
-print('port', app.config['port'])
 
 # Define LOGGING_CONFIG in flask_config.py - path to config file for setting
 # up the logger (e.g. config/logging/local.conf)
