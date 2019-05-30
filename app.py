8a5db99b7f874c59e32e65722fb18d1b6864b986
Joe Kupresanin <joekupresanin@u.northwestern.edu>
2019-05-30 18:05:54 -0500

    Getting year, date into predictions df.

--- app.py
+++ app.py
@@ -26,14 +26,17 @@ db = SQLAlchemy(app)
 def index():
 
     try:
-        predictions = db.session.query(Predictions).filter(Predictions.day == 30).filter(Predictions.month == 5)\
-            .all()
+        dates_avail = db.session.query(Predictions.date).distinct().all()
+        predictions = db.session.query(Predictions).filter(Predictions.day == 30).filter(Predictions.month == 5).all()
         logger.debug("Index page accessed.")
-        return render_template('index.html', predictions=predictions)
+        return render_template('index.html', predictions=predictions, dates_avail=dates_avail)
     except:
         traceback.print_exc()
         logger.warning("Error page accessed.")
         return render_template('error.html')
+
+
+
 
 
 app.run(debug=app.config["DEBUG"], port=app.config["PORT"], host=app.config["HOST"])
