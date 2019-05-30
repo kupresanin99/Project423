7535f9c8ea7041cafff38b892997f72ffb3828e7
Joe Kupresanin <joekupresanin@u.northwestern.edu>
2019-05-30 18:33:47 -0500

    Getting year, date into predictions df.

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def index():
 
     try:
         dates_avail = db.session.query(Predictions.date).distinct().order_by("date").all()
-        predictions = db.session.query(Predictions).filter(Predictions.date == "2019-05-12").all()
+        predictions = db.session.query(Predictions).filter(Predictions.date == "2019-05-29").all()
         logger.debug("Index page accessed.")
         return render_template('index.html', predictions=predictions, dates_avail=dates_avail)
     except:
