fb50b7cad1e28f8247be41346a0b46c3196abe34
Joe Kupresanin <joekupresanin@u.northwestern.edu>
2019-05-31 07:02:30 -0500

    Getting year, date into predictions df.

--- app.py
+++ app.py
@@ -5,19 +5,11 @@ from flask import Flask
 from Create_RDS_DB import Predictions, Reports
 from flask_sqlalchemy import SQLAlchemy
 
-# Initialize the Flask application
 app = Flask(__name__)
-
-# Configure flask app from flask_config.py
 app.config.from_pyfile('config_flask.py')
-
-# Define LOGGING_CONFIG in flask_config.py - path to config file for setting
-# up the logger (e.g. config/logging/local.conf)
 logging.config.fileConfig(app.config["LOGGING_CONFIG"])
 logger = logging.getLogger("baseball")
 logger.debug('Test log')
-
-# Initialize the database
 db = SQLAlchemy(app)
 
 
@@ -44,8 +36,6 @@ def index():
 #         return redirect(url_for('index'))
 #     except:
 #         return render_template('error.html')
-
-
 
 
 app.run(debug=app.config["DEBUG"], port=app.config["PORT"], host=app.config["HOST"])
